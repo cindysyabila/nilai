6093488e567ff6aaace3e7422e6f3f8ab58eb9b4
cindysyabila <cindysyabila05@gmail.com>
2023-10-10 21:22:38 +0700

    Syabila

---
+++ home.php
@@ -0,0 +1,11 @@
+<div>
+    <h2>Kelas TKJ RGI Angkatan XXIX</h2>
+    <img src="gambar/4-removebg.png" alt="tkj" width="50%" class="rounded mx-auto d-block"
+    <p>Artikel adalah karangan faktual secara lengkap dengan panjang</p> 
+        <ul>
+        <li>tertentu yang dibuat untuk dipublikasikan di media daring</li>
+             <li>maupun cetak dan bertujuan menyampaikan gagasan dan fakta yang</li>
+              <li>dapat meyakinkan, mendidik, dan menghibur.</li>
+</ul>
+
+</div>
